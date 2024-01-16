763c74e04d9ea71ff590c85509e2bfbdc5ef06c4
Rita <kapustnikova.rita@bk.ru>
2024-01-16 22:30:43 +0300

    добавлены магические методы

--- src/channel.py
+++ src/channel.py
@@ -12,6 +12,30 @@ class Channel:
         self.__channel_id = channel_id
         self.get_attributes()
 
+    def __str__(self):
+        return f'{self.title} ({self.url})'
+
+    def __add__(self, other):
+        return self.subscriber_count + other.subscriber_count
+
+    def __sub__(self, other):
+        return self.subscriber_count - other.subscriber_count
+
+    def __gt__(self, other):
+        return self.subscriber_count > other.subscriber_count
+
+    def __ge__(self, other):
+        return self.subscriber_count >= other.subscriber_count
+
+    def __lt__(self, other):
+        return self.subscriber_count < other.subscriber_count
+
+    def __le__(self, other):
+        return self.subscriber_count <= other.subscriber_count
+
+    def __eq__(self, other):
+        return self.subscriber_count == other.subscriber_count
+
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         channel = self.get_service().channels().list(id=self.__channel_id, part='snippet,statistics').execute()
@@ -30,9 +54,9 @@ class Channel:
         self.title = channel['items'][0]['snippet']['title']
         self.description = channel['items'][0]['snippet']['description']
         self.url = f'https://www.youtube.com/channel/{self.__channel_id}'
-        self.subscriber_count = channel['items'][0]['statistics']['subscriberCount']
-        self.video_count = channel['items'][0]['statistics']['videoCount']
-        self.view_count = channel['items'][0]['statistics']['viewCount']
+        self.subscriber_count = int(channel['items'][0]['statistics']['subscriberCount'])
+        self.video_count = int(channel['items'][0]['statistics']['videoCount'])
+        self.view_count = int(channel['items'][0]['statistics']['viewCount'])
 
     def to_json(self, file_path):
         channel_info = {
